1280dfd8272fc8ef1b9d9eb887a9419b533c090b
WilliamMatiasDeOliveira <williammatiasjau@hotmail.com>
2025-10-24 20:24:10 -0300

    pequenos ajustes nos dashboard

--- App/Views/Layouts/header.php
+++ App/Views/Layouts/header.php
@@ -13,6 +13,23 @@
     <!-- link bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
+     <!-- link google fonts descritiva ROBOTO -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
+     <!-- link google fonts decorativa LOBSTER -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lobster&family=Quicksand:wght@300..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
+    <!-- favicon -->
+    <link rel="icon" type="image/png" href="assets/imgs/logos/img-logobranca.svg">
+
     <!-- css aplicação -->
      <link rel="stylesheet" href="assets/css/reset.css">
      <link rel="stylesheet" href="assets/css/style.css">
--- App/Views/dashboard-cliente.php
+++ App/Views/dashboard-cliente.php
@@ -85,25 +85,54 @@ if ($user) {
                 </li>
             </ul>
         </div>
-        <div class="sidebar-baixo">
+
+        <div class="sidebar-baixo  d-flex align-items-center">
             <a class="sidebar-perfil">
-                <?php if ($user['foto'] === null): ?>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
-                        class="bi bi-person-square mx-auto mt-1" viewBox="0 0 16 16">
+                <?php if (empty($user['foto'])): ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor"
+                        class="bi bi-person-circle mt-1" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                        <path
-                            d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
+                        <path fill-rule="evenodd"
+                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37
+                      C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
                     </svg>
                 <?php else: ?>
                     <img src="assets/imgs/clientes/<?= $user['foto'] ?>"
-                        alt="Foto de perfil do usuário" width="40" height="40"
-                        class=" mt-1 rounded-circle">
+                        alt="Foto de perfil do usuário" width="50" height="50"
+                        class="mt-1 rounded-circle">
                 <?php endif; ?>
+                <div class="sidebar-nome mt-2">
+                    <p class="mb-0"><?= $user['nome'] ?></p>
+                </div>
+            </a>
+        </div>
+
+
+
+
+
+
+        <!-- <div class="sidebar-baixo flex-collumn align-items-center">
+            <a class="sidebar-perfil">
+                <div class="">
+                    <?php if (empty($user['foto'])): ?>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                        </svg>
+                    <?php else: ?>
+                        <img src="assets/imgs/clientes/<?= $user['foto'] ?>"
+                            alt="Foto de perfil do usuário" width="40" height="40"
+                            class=" mt-1 rounded-circle">
+                    <?php endif; ?>
+                </div>
                 <div class="sidebar-nome">
                     <p><?= $user['nome'] ?></p>
                 </div>
             </a>
-        </div>
+        </div> -->
+
+
     </div>
 
     <div class=" main-dashboard">
@@ -111,9 +140,19 @@ if ($user) {
         <div class="main-content">
             <div class="main-a">
                 <div class="nome-foto">
-                    <img src=" assets/imgs/clientes/<?= $user['foto'] ?>"
-                        alt="Foto de perfil do usuário" width="40" height="40"
-                        class="mx-auto mt-1 rounded-circle">
+                    <div class="d-flex justify-content-center">
+                        <?php if (empty($user['foto'])): ?>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                            </svg>
+                        <?php else: ?>
+                            <img src="assets/imgs/clientes/<?= $user['foto'] ?>"
+                                alt="Foto de perfil do usuário" width="120" height="120"
+                                class=" mt-1 rounded-circle">
+                        <?php endif; ?>
+                    </div>
+
                     <div class="nome-text">
                         <h3><?= $user['nome'] ?></h3>
                         <small>Cliente</small>
