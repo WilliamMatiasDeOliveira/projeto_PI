586346369389e3c51bfc39011d2c24df8e4b1286
lucas-catto <lucascattosilva2024@gmail.com>
2025-11-07 11:11:17 -0300

    Corrigir caminho de carregamento da foto.

--- App/Views/dashboard-cliente.php
+++ App/Views/dashboard-cliente.php
@@ -89,7 +89,7 @@ if ($user) {
                       C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
                     </svg>
                 <?php else: ?>
-                    <img src="assets/imgs/clientes/<?= $user['foto'] ?>"
+                    <img src="http://localhost/projeto_PI/<?= $user['foto'] ?>"
                         alt="Foto de perfil do usuário" width="50" height="50"
                         class="mt-1 rounded-circle">
                 <?php endif; ?>
@@ -120,7 +120,7 @@ if ($user) {
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
                             </svg>
                         <?php else: ?>
-                            <img src="assets/imgs/clientes/<?= $user['foto'] ?>"
+                            <img src="http://localhost/projeto_PI/<?= $user['foto'] ?>"
                                 alt="Foto de perfil do usuário" width="120" height="120"
                                 class=" mt-1 rounded-circle">
                         <?php endif; ?>
